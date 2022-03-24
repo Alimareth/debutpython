86e57feeb6b08379ba48251b6de708b26215986e
Alimareth <alimareth@icloud.com>
2022-03-24 16:21:44 +0100

    first commit

---
+++ Tp1python.py
@@ -0,0 +1,15 @@
+livre = "gatsby le magnifique"
+print(livre)
+nourriture_preferee = "glace"
+nouvelle_nourriture_preferee = "viande"
+nourriture_preferee = "pates"
+print(nourriture_preferee)
+number = 2
+sentences = "python c'est génial"
+booleen = true
+type(number)
+<class 'int'>
+type(sentences)
+<class str>
+type(booleen)
+<class bool>
